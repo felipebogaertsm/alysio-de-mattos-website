38890072632ceb306e473c063a82886543e85318
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-06 08:40:27 -0300

    0.1.17

--- kubernetes/04_wordpress-chart/script.sh
+++ kubernetes/04_wordpress-chart/script.sh
@@ -1,2 +1,2 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install -n alysio-dm -f chart-values.yaml alysio-de-mattos-website bitnami/wordpress
+helm install -n alysio-dm -f kubernetes/04_wordpress-chart/chart-values.yaml alysio-de-mattos-website bitnami/wordpress
